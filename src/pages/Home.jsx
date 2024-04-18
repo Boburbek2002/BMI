0f1c9a4a714e809d14ff418cef2cdcbe2d232765
faxriddin777 <jumaniyozovfaxriddin715@gmail.com>
2024-04-19 00:47:08 +0500

    edit about

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <section className="hero__section">
         <Container>
           <div className="about">
-            <h1>Random Text on Image</h1>
+            <h3>Tahdidlarnini oldini olish usullarini o'rgatishga bag'ishlangan saytimizga xush kelibsiz! Biz shaxslar va tashkilotlarni kibertahdidlar keltirib chiqaradigan xavflarni qanday aniqlash va kamaytirish bo'yicha o'rgatish niyatidamiz</h3>
           </div>
         </Container>
       </section>
--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,123 +1,96 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import { Link } from "react-router-dom";
-import Card from "react-bootstrap/Card";
-import "./services.css";
-import about3 from "./../assets/images/about3.jpg";
-import about2 from "./../assets/images/about2.jpg";
-import about4 from "./../assets/images/about4.jpg";
-import about5 from "./../assets/images/about5.jpg";
-import about6 from "./../assets/images/about6.jpg";
-import about1 from "./../assets/images/about1.jpg";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card'
+import './services.css'
+import about3 from './../assets/images/ddos.jpg';
+import about2 from './../assets/images/ransomware.jpg';
+import about4 from './../assets/images/mitm.jpg';
+import about5 from './../assets/images/sql.jpg';
+import about6 from './../assets/images/zeroday.jpg';
+import about1 from './../assets/images/fishing.jpg'
 
 const Services = () => {
   return (
     <section className="services">
       <Container>
         <Row>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread1">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about1}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about1} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Fishing</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Fishing elektron aloqada ishonchli shaxs sifatida niqoblanib, parollar, kredit karta raqamlari yoki shaxsiy ma'lumotlarni o'g'irlash.
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Link>
+            
           </Col>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread2">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about2}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about2} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Ransomware</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Pul summasi ("to'lov") to'lanmaguncha kompyuter tizimi yoki fayllarga kirishni bloklash uchun mo'ljallangan zararli dastur turi.
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Link>
+            
           </Col>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread3">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about3}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about3} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Xizmatni rad etish</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  DoS va DDoS hujumlari kompyuter tizimi, tarmoq yoki saytni trafik oqimi bilan to'ldirish orqali normal ishlashini buzishga qaratilgan. 
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Link>
+            
           </Col>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread4">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about4}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about4} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Man-in-the-Middle (O'rtadagi odam) </Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  O'rtadagi odam hujumlari tajovuzkor ikki tomon o'rtasida aloqani to'xtatganda va potensial ravishda o'zgartirganda sodir bo'ladi. 
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Link>
           </Col>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread5">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about5}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about5} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>SQL Inyeksiya</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  SQL Inyeksiya - bu kirish maydonlariga zararli SQL kodini kiritish orqali veb-ilovalarga qaratilgan kiberhujum turi.
                   </Card.Text>
                 </Card.Body>
               </Card>
-            </Link>
+            </Link>            
           </Col>
-          <Col lg="4" md="4">
+          <Col lg='4' md='4'>
             <Link to="/thread6">
-              <Card style={{ width: "100%", marginBottom: "20px" }}>
-                <Card.Img
-                  variant="top"
-                  src={about6}
-                  style={{ height: "11.5rem" }}
-                />
+              <Card style={{ width: '100%', marginBottom: '20px' }}>
+                <Card.Img variant="top" src={about6} style={{height: '11.5rem'}} />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>Nolinchi kun</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                  Nolinchi kun hujumi sotuvchi yoki jamoatchilikka hali ma'lum bo'lmagan dasturiy yoki apparatdagi zaifliklarga qaratilgan. 
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -126,7 +99,7 @@ const Services = () => {
         </Row>
       </Container>
     </section>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
